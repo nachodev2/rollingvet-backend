b61504f69674f826e6e17e43685de306e16872c9
Tomas Ruiz <ruiztms20@gmail.com>
2025-10-22 04:01:30 -0300

    Ahora los turnos creados por usuarios se muestran correctamente en administraicon

--- routes/turnoRoutes.js
+++ routes/turnoRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { crearTurno, obtenerTurnos, actualizarTurno, eliminarTurno } = require('../controllers/turnoController');
+const { protect, authorize } = require('../controllers/auth');
 
+router.post('/', protect, crearTurno); // Users can create own turnos
 
-router.post('/', crearTurno);
-
-router.get('/', obtenerTurnos);
+router.get('/', protect, authorize('admin'), obtenerTurnos); // Only admin see all
 
 router.route('/:id')
-    .put(actualizarTurno)
-    .delete(eliminarTurno);
+    .put(protect, authorize('admin'), actualizarTurno)
+    .delete(protect, authorize('admin'), eliminarTurno);
 
 module.exports = router;
